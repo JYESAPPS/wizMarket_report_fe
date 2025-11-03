3bdf0c1d414d090d98000203e3935ff74056d9f9
ddogooni <seokun1229@gmail.com>
2025-11-03 11:11:50 +0900

    11.3 wizReport 수정

--- src/pages/Report/Component/AdviceSummary.jsx
+++ src/pages/Report/Component/AdviceSummary.jsx
@@ -35,7 +35,7 @@ const AdviceSummary = ({ risingBusiness, population }) => {
   if (!risingAdvice && !locAdvice && !populationAdvice) return null;
 
   return (
-    <div className="bg-white p-4 rounded-md tracking-tight shadow-md shadow-black-500">
+    <div className="text-2xl bg-white p-4 rounded-md tracking-tight shadow-md shadow-black-500">
       <AdviceBlock title="업종 트랜드 분석 및 조언" html={risingAdvice} showIcon />
       <AdviceBlock title="지역경제 특성에 따른 장사 전략 요약을 확인하세요." html={locAdvice} showIcon={false} />
       <AdviceBlock title="연령별 특성 및 응대방법을 제안드립니다." html={populationAdvice} showIcon={false} />
