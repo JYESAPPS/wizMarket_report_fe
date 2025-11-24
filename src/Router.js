d980c6889879ad0e79657a3e8ed5067f4e834091
tpals213 <tpals213@gmail.com>
2025-11-24 15:45:51 +0900

    라우터 원복

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+/* element 값에 넣으면됨. 
+ReportCopy.jsx : 리포트 수정전 원본파일
+Report.jsx : 풀무원 커스텀
+WizReport.jsx : Wiz App 커스텀
+*/
+
+// import Report from "./pages/Report/Report";             
+// import ReportCopy from "./pages/Report/ReportCopy";
 import WizReport from "./pages/Report/WizReport";
 
 const Router = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        {/* 해시 기준 경로: #/report/:uuid */}
-        <Route path="/report/:uuid" element={<WizReport />} />
+        <Route path="/wizmarket/report/:uuid" element={<WizReport />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
