55298fdfa4c9f8dfeb1aac9c10a190c8bdcbf3f6
tpals213 <tpals213@gmail.com>
2025-11-24 15:24:09 +0900

    해시 라우터 설정

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-/* element 값에 넣으면됨. 
-ReportCopy.jsx : 리포트 수정전 원본파일
-Report.jsx : 풀무원 커스텀
-WizReport.jsx : Wiz App 커스텀
-*/
-
-// import Report from "./pages/Report/Report";             
-// import ReportCopy from "./pages/Report/ReportCopy";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import WizReport from "./pages/Report/WizReport";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/wizmarket/report/:uuid" element={<WizReport />} />
+        {/* 해시 기준 경로: #/report/:uuid */}
+        <Route path="/report/:uuid" element={<WizReport />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
